115aa5f3cc8c57c4b30a77eef4fe6972b5272ac3
WillMS <aquatixo@naver.com>
2023-02-20 15:06:16 +0900

    Initial commit

---
+++ src/main/java/willms/ProjectShopSysApplication.java
@@ -0,0 +1,13 @@
+package willms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShopSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShopSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/willms/ProjectShopSysApplicationTests.java
@@ -0,0 +1,13 @@
+package willms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShopSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
