674f20fa4dc5b8017463521940380cce0aa1de82
WillMS <aquatixo@naver.com>
2023-02-21 23:01:50 +0900

    member con start

--- src/main/java/willms/member/controller/MemberController.java
+++ src/main/java/willms/member/controller/MemberController.java
@@ -1,6 +1,36 @@
 package willms.member.controller;
 
-public class MemberController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import willms.member.dto.MemberRequestDto;
+import willms.member.dto.MemberResponseDto;
+import willms.member.service.MemberService;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponseDto> getMyMemberInfo() {
+        MemberResponseDto myInfoBySecurity = memberService.getMyInfoBySecurity();
+        System.out.println(myInfoBySecurity.getNickname());
+        return ResponseEntity.ok((myInfoBySecurity));
+        // return ResponseEntity.ok(memberService.getMyInfoBySecurity());
+    }
+
+    @PostMapping("/me2")
+    public ResponseEntity<MemberResponseDto> getMyMemberInfo2() {
+        MemberResponseDto myInfoBySecurity = memberService.getMyInfoBySecurity();
+        System.out.println(myInfoBySecurity.getNickname());
+        return ResponseEntity.ok((myInfoBySecurity));
+    }
+
+    @PostMapping("/nickname")
+    public ResponseEntity<MemberResponseDto> setMemberNickname(@RequestBody MemberRequestDto request) {
+        return ResponseEntity.ok(memberService.changeMemberNickname(request.getEmail(), request.getNickname()));
+    }
 
 }
